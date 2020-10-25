d1c625ded8ed356b00c9e1c58c23568d6306699e
karianov <karianov@correo.udistrital.edu.co>
2020-10-24 21:23:27 -0500

    Change INNER JOIN with LEFT JOIN in SQL query

--- car-center-api/src/main/java/co/org/karianov/carcenterapi/jpa/repo/MecanicoRepo.java
+++ car-center-api/src/main/java/co/org/karianov/carcenterapi/jpa/repo/MecanicoRepo.java
@@ -10,7 +10,7 @@ import co.org.karianov.carcenterapi.model.entity.Mecanico;
 
 public interface MecanicoRepo extends JpaRepository<Mecanico, MecanicoId> {
 
-	@Query(value = "SELECT mec.tipo_documento, mec.documento, mec.primer_nombre, mec.segundo_nombre, mec.primer_apellido, mec.segundo_apellido, mec.celular, mec.direccion, mec.email, mec.estado, (SELECT COALESCE(SUM(ser.tiempo_estimado), 0) FROM car_center_db.servicio_x_mantenimiento AS ser WHERE ser.cod_mantenimiento = man.codigo  AND man.fecha < DATE_SUB(NOW(), INTERVAL 1 MONTH)) AS horas FROM car_center_db.mecanico AS mec INNER JOIN car_center_db.mantenimiento AS man ON mec.tipo_documento = man.mec_tipo_documento AND mec.documento = man.mec_documento WHERE mec.estado = 'D' ORDER BY horas DESC LIMIT 10", nativeQuery = true)
+	@Query(value = "SELECT mec.tipo_documento, mec.documento, mec.primer_nombre, mec.segundo_nombre, mec.primer_apellido, mec.segundo_apellido, mec.celular, mec.direccion, mec.email, mec.estado, (SELECT COALESCE(SUM(ser.tiempo_estimado), 0) FROM car_center_db.servicio_x_mantenimiento AS ser WHERE ser.cod_mantenimiento = man.codigo  AND man.fecha < DATE_SUB(NOW(), INTERVAL 1 MONTH)) AS horas FROM car_center_db.mecanico AS mec LEFT JOIN car_center_db.mantenimiento AS man ON mec.tipo_documento = man.mec_tipo_documento AND mec.documento = man.mec_documento WHERE mec.estado = 'D' ORDER BY horas DESC LIMIT 10", nativeQuery = true)
 	public List<Mecanico> consultarMecanicosDisponibles();
 
 }
